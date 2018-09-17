5368e7127cd9f76b63386cb665b9130a63031f10
yuerenyuan <945155148@qq.com>
2018-09-17 20:44:56 +0800

    add Dog.java

---
+++ src/main/java/com/example/demo/git/Dog.java
@@ -0,0 +1,10 @@
+package com.example.demo.git;
+
+/**
+ * Created by tanke on 2018/9/17.
+ */
+public class Dog {
+    public static void main(String[] args) {
+        System.out.println("坦克");
+    }
+}
