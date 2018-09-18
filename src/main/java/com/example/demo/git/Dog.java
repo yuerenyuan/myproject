ab2f62432459c8a3813c6598249ff776b390ea6d
yuerenyuan <945155148@qq.com>
2018-09-18 17:10:42 +0800

    updata

--- src/main/java/com/example/demo/git/Dog.java
+++ src/main/java/com/example/demo/git/Dog.java
@@ -3,9 +3,27 @@ package com.example.demo.git;
 /**
  * Created by tanke on 2018/9/17.
  */
-public class Dog {
+public class Dog<T> {
+    private T object;
+
+    public Dog(T object) {
+        this.object = object;
+    }
+
+    public T getObject() {
+        return object;
+    }
+
+    public void setObject(T object) {
+        this.object = object;
+    }
+
     public static void main(String[] args) {
-        System.out.println("坦克");
-        System.out.println("不是坦克");
+        Dog<String> dog1=new Dog<String>("tanKe");
+        Dog<Integer> dog2=new Dog<Integer>(66);
+        String a=dog1.getObject();
+        Integer b=dog2.getObject();
+        System.out.println(a);
+        System.out.println(b);
     }
 }
